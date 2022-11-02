cb9d6fa8d55410b4ace40ce9f6fca66aab350a7d
Soussi Mohamed Nour <soussi@noreply-github.com>
2022-11-02 12:39:02 +0100

    install needed dependencies

---
+++ Cross-Compiler/dependencies.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+OS="$(uname -s)"
+
+declare -a debian_dependencies=(
+  "build-essential"
+  "bison"
+  "flex"
+  "libgmp3-dev"
+  "libmpc-dev"
+  "libmpfr-dev"
+  "texinfo"
+  "libcloog-isl-dev"
+  "libisl-dev"
+)
+
+declare -a fedore_dependencies=(
+  "gcc"
+  "gcc-c++"
+  "make"
+  "bison"
+  "flex"
+  "gmp-devel"
+  "libmpc-devel"
+  "mpfr-devel"
+  "texinfo"
+  "cloog-devel"
+  "isl-devel"
+)
+
+declare -a arch_dependencies=(
+  "base-devel"
+  "gmp"
+  "libmpc"
+  "mpfr"
+)
+
+function detect_os()
+{
+  case "$OS" in
+    Linux)
+      if [ -f "/etc/arch-release" ] || [ -f "/etc/artix-release" ];then
+        echo "OS is Arch based"
+        sudo pacman -S "${arch_dependencies[@]}"
+      elif [ -f "/etc/fedora-release" ] || [ -f "/etc/redhat-release" ]; then
+        RECOMMEND_INSTALL="sudo dnf install -y"
+      else # assume debian based
+        RECOMMEND_INSTALL="sudo apt install -y"
+      fi
+    ;;
+    *)
+      echo "The operating System "$OS" is currently not supported"
+      exit 1
+    ;;
+  esac
+}
+detect_os
